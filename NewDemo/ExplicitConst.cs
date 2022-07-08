dfe8d736869a7721cb2fb9b9b0eeae38db042165
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 18:09:49 +0530

    Add project files.

---
+++ NewDemo/ExplicitConst.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewDemo
+{
+    internal class ExplicitConst
+    {
+        public static int sv;
+        public int iv;
+
+        //explicit constructor
+        public ExplicitConst()
+        {
+            sv = 12;
+            Console.WriteLine("Explicit constructor");
+            Console.WriteLine("values---- = " + sv);
+        }
+
+        //explicit constructor -parameterized
+        public ExplicitConst(int i)
+        {
+            iv=i;
+            
+        }
+
+
+        //constructor - static - executes before main - called only once
+        static ExplicitConst()
+        {
+            sv=10;
+            Console.WriteLine("static constructor executed and value = "+sv); 
+        }
+
+
+        //explicit constructor -copy constructor
+        public ExplicitConst(ExplicitConst obj)
+        {
+            iv = obj.iv;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("value = " + iv);
+        }
+
+    }
+}
---
+++ NewDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewDemo
+{
+    internal class Program
+    {
+        //implicit constructor
+        int i;
+        bool b;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Program p = new Program();
+            Console.WriteLine("Hello = "+p.i);
+            Console.WriteLine("Hello = "+p.b);
+            ExplicitConst obj = new ExplicitConst();
+            ExplicitConst obj1 = new ExplicitConst();
+            ExplicitConst obj2 = new ExplicitConst();
+            ExplicitConst obj3 = new ExplicitConst(2);
+            ExplicitConst obj4 = new ExplicitConst(obj3);
+            ExplicitConst obj5 = new ExplicitConst(obj3);
+            obj4.display();
+            obj3.display();
+
+
+            //diff between variable , instance, reference
+            ExplicitConst varobj1; //variable of class
+            varobj1 = new ExplicitConst(); //instance of class
+            ExplicitConst varobj2 = varobj1; //reference of class - pointer to varobj1
+            varobj1.iv = 20;
+            Console.WriteLine("--value = "+ varobj1.iv);
+            Console.WriteLine("--value = "+ varobj2.iv);
+            varobj2.iv = 40;
+            Console.WriteLine("--value = " + varobj1.iv);
+            Console.WriteLine("--value = " + varobj2.iv);
+
+            Console.ReadLine();
+
+           
+        }
+    }
+}
